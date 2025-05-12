7afee77bebbbc1fe227e3efa299cfca50e68ebfd
Gaurav Burjwal <gaurav.burjwal@ridecell.com>
2025-05-12 15:28:05 +0530

    add dist

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,22 @@
-
-import React from 'react';
+import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-const container = document.getElementById('root');
+const renderMyApp = elementId => {
+  createRoot(document.getElementById(elementId)).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+};
 
-if (!container) {
-  throw new Error('Failed to find the root element');
+declare global {
+  interface Window {
+    mount: (elementId: string) => void;
+  }
 }
 
-const root = createRoot(container);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+window.mount = renderMyApp;
+
+// renderMyApp('root');
